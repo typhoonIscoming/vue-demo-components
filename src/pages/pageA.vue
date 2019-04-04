49a5258713ab65407271b48d97559427023c200d
xiehang <850525344@qq.com>
2019-04-04 14:34:29 +0800

    save

--- src/pages/pageA.vue
+++ src/pages/pageA.vue
@@ -61,32 +61,15 @@ export default {
   },
   methods: {
     selectEvent() {
-      console.log(111111)
       this.showList = true
     },
     test() {
-      this.index +=1
-      const fn = this.debounce(() => {
-        this.showTimes +=1
-      })
-      fn()
+      this.index +=1;
+      this.showTimesDebounce(123)
     },
-    debounce(fn, delay = 300) {
-    // 定时器，用来 setTimeout
-    var context = this
-    // 返回一个函数，这个函数会在一个时间区间结束后的 delay 毫秒时执行 fn 函数
-    return function () {
-        // 保存函数调用时的上下文和参数，传递给 fn
-        console.log('this.timer', context.timer)
-        // 每次这个返回的函数被调用，就清除定时器，以保证不执行 fn
-        clearTimeout(context.timer)
-        // 当返回的函数被最后一次调用后（也就是用户停止了某个连续的操作），
-        // 再过 delay 毫秒就执行 fn
-        context.timer = setTimeout(function () {
-            fn.apply(context, arguments)
-        }, delay)
-    }
-}
+    showTimesDebounce: debounce(function(){
+      this.showTimes +=1;
+    }),
   },
   watch: {
     selected(newvalue) {
