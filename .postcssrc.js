1ff5904d393af3cde04e5a5e0a3bc2af5afe35fb
xiehang <xiehang@encdata.cn>
2019-08-10 15:40:19 +0800

    save

---
+++ .postcssrc.js
@@ -0,0 +1,12 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+    "plugins": {
+        "postcss-import": {},
+        "postcss-url": {},
+        "autoprefixer": {},
+        // "postcss-px2rem": {
+        //     remUnit: 14
+        // },
+    }
+}
