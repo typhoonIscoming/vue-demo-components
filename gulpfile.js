7ed2596406a71bd56ebc51ddce79fbabc060a0cf
xiehang <xiehang@encdata.cn>
2019-10-18 17:24:36 +0800

    format  gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -15,58 +15,58 @@ const SPRITE_DEST = path.resolve(__dirname, 'src/assets/sprites')
 // 生成的scss文件根根目录
 const SPRITE_SCSS = path.resolve(__dirname, 'src/theme/sprites')
 
-if(!fs.existsSync(SPRITE_DEST)) {
-  // child_process.execSync(`mkdir -p ${SPRITE_DEST}`)
-  mkdirp.sync(SPRITE_DEST)
+if (!fs.existsSync(SPRITE_DEST)) {
+    // child_process.execSync(`mkdir -p ${SPRITE_DEST}`)
+    mkdirp.sync(SPRITE_DEST)
 }
-if(!fs.existsSync(SPRITE_SCSS)) {
-  // child_process.execSync(`mkdir -p ${}`)
-  mkdirp.sync(SPRITE_SCSS)
+if (!fs.existsSync(SPRITE_SCSS)) {
+    // child_process.execSync(`mkdir -p ${}`)
+    mkdirp.sync(SPRITE_SCSS)
 }
 
 gulp.task('default', function () {
-  readDirSync(ICON_SOURCE)
+    readDirSync(ICON_SOURCE)
 
-  function readDirSync(dir) {
-    const files = fs.readdirSync(dir)
-    const pngs = files.filter(file => /\.png/.test(file))
-    if(pngs.length >= 1) {
-      // 获取当前文件相对于ICON_SOURCE的路径
-      const relativePath = path.relative(ICON_SOURCE, dir)
-      const spirteDest = path.resolve(SPRITE_DEST, relativePath)
-      // scss文件中引用的sprite图片地址
-      const scssImgDest = path.relative(__dirname, spirteDest).replace(/^src\//ig, '~')
-      // scss文件存放的地址
-      const scssDest = path.resolve(SPRITE_SCSS, relativePath)
-      gulp.src(`${dir}/*.png`)
-        .pipe(spritesmith({
-            imgName: 'sprite.png',
-            cssName: 'sprite.scss',
-            padding: 4,
-            imgPath: `${scssImgDest}/sprite.png`,
-            cssTemplate: 'gulp_templates/scss.2x.template.handlebars',
-        }))
-        .pipe(gulp.dest(spirteDest))
-        .on('finish', () => {
-            const tmpSrc = path.join(spirteDest, 'sprite.scss')
-            const tmpDst = path.join(scssDest, 'sprite.scss')
-            if(!fs.existsSync(scssDest)) mkdirp.sync(scssDest)
-            fs.renameSync(tmpSrc, tmpDst)
+    function readDirSync(dir) {
+        const files = fs.readdirSync(dir)
+        const pngs = files.filter(file => /\.png/.test(file))
+        if (pngs.length >= 1) {
+            // 获取当前文件相对于ICON_SOURCE的路径
+            const relativePath = path.relative(ICON_SOURCE, dir)
+            const spirteDest = path.resolve(SPRITE_DEST, relativePath)
+            // scss文件中引用的sprite图片地址
+            const scssImgDest = path.relative(__dirname, spirteDest).replace(/^src\//ig, '~')
+            // scss文件存放的地址
+            const scssDest = path.resolve(SPRITE_SCSS, relativePath)
+            gulp.src(`${dir}/*.png`)
+                .pipe(spritesmith({
+                    imgName: 'sprite.png',
+                    cssName: 'sprite.scss',
+                    padding: 4,
+                    imgPath: `${scssImgDest}/sprite.png`,
+                    cssTemplate: 'gulp_templates/scss.2x.template.handlebars',
+                }))
+                .pipe(gulp.dest(spirteDest))
+                .on('finish', () => {
+                    const tmpSrc = path.join(spirteDest, 'sprite.scss')
+                    const tmpDst = path.join(scssDest, 'sprite.scss')
+                    if (!fs.existsSync(scssDest)) mkdirp.sync(scssDest)
+                    fs.renameSync(tmpSrc, tmpDst)
+                })
+        }
+
+        files.forEach(function (ele) {
+            const curPath = `${dir}/${ele}`
+            const info = fs.statSync(curPath)
+            if (info.isDirectory()) {
+                return readDirSync(curPath)
+            }
         })
     }
-
-    files.forEach(function(ele){
-      const curPath = `${dir}/${ele}`
-      const info = fs.statSync(curPath)
-      if(info.isDirectory()){
-        return readDirSync(curPath)
-      }
-    })
-  }
 })
 
 gulp.task('clearSprite', function () {
-  // child_process.execSync(`rm -rf ${SPRITE_DEST} ${SPRITE_SCSS}`)
-  rmfr(SPRITE_DEST)
-  rmfr(SPRITE_SCSS)
+    // child_process.execSync(`rm -rf ${SPRITE_DEST} ${SPRITE_SCSS}`)
+    rmfr(SPRITE_DEST)
+    rmfr(SPRITE_SCSS)
 })
