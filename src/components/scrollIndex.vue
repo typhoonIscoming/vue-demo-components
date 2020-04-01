92eeab051035d4349f9dee68d1b5b9106267d980
xiehang <xiehang@encdata.cn>
2020-04-01 18:24:08 +0800

    scroll index

---
+++ src/components/scrollIndex.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="enc-scroll-to-index">
+        <div class="xh-index-nav">
+            <ul class="xh-indexlist-navlist">
+                <li
+                    v-for="(section, index) in groupList"
+                    :key="index"
+                    :class="{'selected-index-nav': section === currentIndicator}"
+                >
+                    {{ section }}
+                </li>
+            </ul>
+        </div>
+        <div class="scroll-list-content">
+            <slot />
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'EncScrollIndex',
+    data() {
+        return {
+            currentIndicator: null,
+        }
+    },
+    props: {
+        groupList: {
+            type: Array,
+            default: () => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        },
+    },
+    methods: {
+
+    },
+}
+</script>
+<style lang="scss" scoped>
+.enc-scroll-to-index{
+
+}
+</style>
--- src/pages/pageD.vue
+++ src/pages/pageD.vue
@@ -2,7 +2,8 @@
     <div class="D common page-d-container">
         <common title="d"></common>
         <div class="content">
-            <div id="container"></div>
+            <xh-scroll-index>
+            </xh-scroll-index>
         </div>
         <div class="bottom">D页面</div>
     </div>
@@ -10,66 +11,26 @@
 
 <script>
 import common from "@/components/common";
-import G6 from '@antv/g6';
+import scrollIndex from '@/components/scrollIndex';
 
 export default {
     data() {
         return {
-            graph: null,
-            initData: {
-                nodes: [
-                    { id: 'node1', label: 'Circle1', x: 150, y: 150 },
-                    { id: 'node2', label: 'Circle2', x: 400, y: 150 }
-                ],
-                edges: [
-                    { source: 'node1', target: 'node2' }
-                ]
-            }
+            list: [
+                
+            ],
         };
     },
     components: {
-        common
+        common,
+        'xh-scroll-index': scrollIndex,
     },
     created() {
         
     },
     mounted() {
-        this.initG6()
     },
     methods: {
-        initG6() {
-            const width = document.getElementById('container').clientWidth;
-            const height = document.getElementById('container').clientHeight;
-            console.log(width, height)
-            this.graph = new G6.Graph({
-                container: 'container',
-                width,
-                height,
-                defaultNode: {
-                    shape: 'rect',
-                    size: [ 100 ],
-                    color: '#5B8FF9',
-                    style: {
-                    fill: '#9EC9FF',
-                    lineWidth: 3
-                    },
-                    labelCfg: {
-                    style: {
-                        fill: '#fff',
-                        fontSize: 20
-                    }
-                    }
-                },
-                defaultEdge: {
-                    style: {
-                    stroke: '#e2e2e2'
-                    }
-                }
-            })
-            this.graph.data(this.initData);
-            this.graph.render();
-            console.log(this.graph.getEdges())
-        },
     },
 };
 </script>
