fc487dc9af189eea939f9a9af918206f8762f79c
xiehang <xiehang@encdata.cn>
2020-06-04 10:59:22 +0800

    测试各种插件

--- src/pages/pageF.vue
+++ src/pages/pageF.vue
@@ -1,20 +1,29 @@
 <template>
     <div class="container common f-page">
         <xh-common title="f" />
+        <pdf
+			src="https://ecard-test.95505.cn:8082/upload/20200525/2fbc872ac4cc4e42b383aa95c3fa679c.pdf"
+			@num-pages="pageCount = $event"
+			@page-loaded="currentPage = $event"
+		></pdf>
     </div>
 </template>
 
 
 <script>
 import common from "@/components/common";
+import pdf from 'vue-pdf'
+
 export default {
     data() {
         return {
-
+            currentPage: 0,
+			pageCount: 0,
         }
     },
     components: {
         'xh-common': common,
+        pdf,
     }
 }
 </script>
