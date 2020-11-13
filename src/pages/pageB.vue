77deda4dc3b437a551bc6ccbb6b78f986b52d300
xiehang <xiehang@encdata.cn>
2020-11-13 18:10:18 +0800

    引入wangEditor

--- src/pages/pageB.vue
+++ src/pages/pageB.vue
@@ -2,14 +2,14 @@
     <div class="B common">
         <common title="b"></common>
         <div class="content">
-            <a :href="'sms:' + linkPeople">{{ $t('m.sendSMS') }}</a>
-            <select-item
-                class="select-item"
-                @click.native="comEvent"/>
-            <child-one />
-            <child-two />
-            <child-three :text = linkPeople />
-            <child-four />
+            <!-- <a :href="'sms:' + linkPeople">{{ $t('m.sendSMS') }}</a> -->
+            <div class="editor-container">
+                <!-- <div id="toolbar-container" class="toolbar"></div> -->
+                <div id="editor" class="text-container"></div>
+            </div>
+            <div class="operator">
+                <button @click="getdata">get Json</button>
+            </div>
         </div>
         <div class="bottom">
             b{{ $t('m.page') }}
@@ -18,6 +18,7 @@
 </template>
 
 <script>
+import E from 'wangeditor'
 import common from '@/components/common';
 import selectItem from '@/components/pageB/selected'
 
@@ -25,21 +26,34 @@ export default {
     data() {
         return {
             linkPeople: '18502178364',
+            editor: null,
         };
     },
     components: {
         common,
         'select-item': selectItem,
     },
-    methods: {
-        comEvent() {
-            console.log('this fathers components')
-        },
-    },
     created() {
         const str = '13558513096'
         const reg = /^(\d{3})\d{4}(\d{4})$/;
         console.log('telphone hide', str.replace(reg, '$1****$2'))
+    },
+    mounted() {
+        if (!this.editor) {
+            this.editor = new E('#editor')
+            this.editor.config.showLinkImg = false; // 即可隐藏插入网络图片的功能，即只保留上传本地图片
+            this.editor.config.linkCheck = function linkCheck(text, link) {
+                console.log(text, link)
+                return true
+            }
+            this.editor.create()
+        }
+    },
+    methods: {
+        getdata() {
+            const json = this.editor.txt.getJSON()
+            console.log('json', json)
+        },
     },
 };
 </script>
@@ -47,9 +61,16 @@ export default {
 <style lang='scss' scoped>
 @import '@/css/common.scss';
 .B{
-  .select-item{
-    border: 1px solid black;
-    box-sizing: border-box;
-  }
+    .select-item{
+        border: 1px solid black;
+        box-sizing: border-box;
+    }
+    .editor-container{
+        padding: 50px;
+        box-sizing: border-box;
+    }
+    .text-container{
+        height: 300px;
+    }
 }
 </style>
---
+++ src/utils/editorExtend.js
@@ -0,0 +1,33 @@
+import E from 'wangeditor'
+
+class AlertMenu extends BtnMenu {
+    constructor(editor) {
+        const $elem = E.$(
+            `<div class="w-e-menu">
+                <button>alert</button>
+            </div>`
+        )
+        super($elem, editor)
+    }
+    // 菜单点击事件
+    clickHandler() {
+        // 做任何你想做的事情
+        // 可参考【常用 API】文档，来操作编辑器
+
+        console.log('hello world')
+    }
+    // 菜单是否被激活（如果不需要，这个函数可以空着）
+    // 1. 激活是什么？光标放在一段加粗、下划线的文本时，菜单栏里的 B 和 U 被激活，如下图
+    // 2. 什么时候执行这个函数？每次编辑器区域的选区变化（如鼠标操作、键盘操作等），都会触发各个菜单的 tryChangeActive 函数，重新计算菜单的激活状态
+    tryChangeActive() {
+        // 激活菜单
+        // 1. 菜单 DOM 节点会增加一个 .w-e-active 的 css class
+        // 2. this.this.isActive === true
+        this.active()
+
+        // // 取消激活菜单
+        // // 1. 菜单 DOM 节点会删掉 .w-e-active
+        // // 2. this.this.isActive === false
+        // this.unActive()
+    }
+}
