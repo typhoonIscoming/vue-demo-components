c66a10b4d6950eb23adb04743f7beef9d3ebf39f
typhoonIscoming <850525344@qq.com>
2021-12-19 23:45:27 +0800

    mod: 暂存

--- src/modules/subAaPage/MacMenu.vue
+++ src/modules/subAaPage/MacMenu.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="MacMenuContainer" @mousemove.stop="handleHover">
-        <div class="MenuItem" v-for="item in 8" :key="item">
-            {{ item }}
+        <div class="content">
+            <div class="MenuItem" v-for="item in 8" :key="item">
+                {{ item }}
+            </div>
         </div>
     </div>
 </template>
@@ -24,13 +26,15 @@ export default {
             moveFn(e)
         },
         move(e) {
-            const { x, y } = e;
-            const container = document.querySelector('.MacMenuContainer')
+            const { x } = e;
+            const container = document.querySelector('.content')
             const children = [].slice.call(container.children)
             children.forEach((item) => {
-                console.log('item', item.offsetLeft)
+                const { width, x: itemX } = item.getBoundingClientRect();
+                const itemCenterXPola = itemX + width / 2;
+                const diff = Math.abs(x - itemCenterXPola);
+                console.log('itemCenterXPola', diff)
             })
-            console.log('e', x, y, children)
         },
     },
 }
@@ -40,18 +44,24 @@ export default {
     display: inline-block;
     margin: 50px auto 0;
     position: relative;
+    cursor: pointer;
+    padding: 10px;
+    background: #F3D7D1;
+    border-radius: 20px;
+    .content{
+        display: flex;
+    }
     .MenuItem{
         width: 50px;
         height: 50px;
         line-height: 50px;
         text-align: center;
-        float: left;
         margin: 0 13px;
         transition: all .2s linear;
         background: greenyellow;
-        border-radius: 100%;
+        border-radius: 10px;
+        transform-origin: bottom center;
         &:hover{
-            transform-origin: bottom center;
             transform: scale(1.5);
         }
     }
